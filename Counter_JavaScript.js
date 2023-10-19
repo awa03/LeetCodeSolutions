61d9f2402031d6dbe61a8d528bb754fddcb33dbc
Aiden <118379307+awa03@users.noreply.github.com>
2023-10-19 01:34:00 -0400

    Create Counter_JavaScript.js

---
+++ Counter_JavaScript.js
@@ -0,0 +1,19 @@
+
+
+# Code
+```
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    return ()=> n++
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
+```
