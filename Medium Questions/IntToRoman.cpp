cd2fd87d671c9271a61314a0cadcfed61b4a77c9
Aiden <118379307+awa03@users.noreply.github.com>
2023-12-20 13:45:24 -0500

    Create IntToRoman.cpp

---
+++ Medium Questions/IntToRoman.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+       string Roman = "";
+       vector<pair<int, string>> storeIntRoman = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
+       for(int i = 0; i < storeIntRoman.size(); i++){ // check all combos
+           while(num >= storeIntRoman[i].first){
+               Roman += storeIntRoman[i].second; // add its associated val
+               num-= storeIntRoman[i].first; // make num smaller
+           }
+       }
+       return Roman;
+    }
+};
