fc2634f5bfbd8bb9a9505f8d409be70ad9a595a0
Aiden <118379307+awa03@users.noreply.github.com>
2024-04-07 13:22:06 -0400

    Create Pow.py

---
+++ Pow.py
@@ -0,0 +1,10 @@
+import math
+class Solution(object):
+    def myPow(self, x, n):
+        return math.pow(x, n)
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        
