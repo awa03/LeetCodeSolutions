683457c9747919f8ccfa0ae15e707e435f05cc22
Aiden <118379307+awa03@users.noreply.github.com>
2024-09-24 16:53:04 -0400

    Create Roman_To_Int.rs

---
+++ Easy Questions/Roman_To_Int.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn roman_to_int(s: String) -> i32 {
+        let s_translated = s
+            .replace("IV", "IIII")
+            .replace("IX", "VIIII")
+            .replace("XL", "XXXX")
+            .replace("XC", "LXXXX")
+            .replace("CD", "CCCC")
+            .replace("CM", "DCCCC");
+
+        s_translated.chars().map(|c| {
+            match c {
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+                _ => 0,
+            }
+        }).sum()
+    }
+}
