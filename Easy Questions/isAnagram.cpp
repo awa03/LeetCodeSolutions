dc300928d4ba2c97c8fd1d569bf64149ee4a0661
Aiden <118379307+awa03@users.noreply.github.com>
2024-11-06 15:35:53 -0500

    Update isAnagram.cpp

--- Easy Questions/isAnagram.cpp
+++ Easy Questions/isAnagram.cpp
@@ -17,3 +17,22 @@ public:
         
     }
 };
+
+// this is an easyyyyy problem
+// lets use one cache, increase when found in s
+// decrease when found in t , voting algorithm 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> cache;
+        if(s.size() != t.size()) return false;
+        for(int i = 0; i < s.size(); i++){
+            cache[s[i]]++;
+            cache[t[i]]--;
+        }
+        for(auto& entries : cache) {
+            if(entries.second != 0) return false;
+        }
+        return true;
+    }
+};
